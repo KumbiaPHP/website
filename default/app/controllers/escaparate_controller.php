7cfc6075efe8e6e277ce6462e6609a4981e67e5a
joanhey <joanhey@kumbiaphp.com>
2012-10-18 13:37:42 +0200

    Pasado el escaparate a su propio controller. Ya que será necesario. Posiblemente despues moveremos páginas al pages_controller

---
+++ default/app/controllers/escaparate_controller.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Controller para crear un escaparate de webs en KumbiaPHP
+ * 
+ */
+class EscaparateController extends AppController {
+	
+	
+	public function index()	{
+		
+	}
+	
+	public function categoria() {
+	
+	}
+
+	public function tag(){
+		
+	}	
+	
+	
+	public function nueva() {
+		
+	}
+	
+}
